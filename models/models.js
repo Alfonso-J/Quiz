33f18fbadbe554c19123bc55ea25329bb50a1ea4
Alfonso <macalf@MacBook-Air-de-MacAlf.local>
2015-07-04 11:31:13 +0200

    corregido error modelo

--- models/models.js
+++ models/models.js
@@ -37,16 +37,20 @@ sequelize.sync().then(function() {
   Quiz.count().then(function (count){
      if(count === 0) {   // la tabla se inicializa solo si está vacía
        Quiz.create({ pregunta: 'Capital de Italia',
-       	            respuesta: 'Roma'
+       	            respuesta: 'Roma',
+                    tema: 'otro'
        	         });
         Quiz.create({ pregunta: 'Capital de Portugal',
-        	            respuesta: 'Lisboa'
+        	            respuesta: 'Lisboa',
+                      tema: 'otro'
         	         });
          Quiz.create({ pregunta: 'Capital de Francia',
-         	            respuesta: 'Paris'
+         	            respuesta: 'Paris',
+                       tema: 'otro'
          	         });
         Quiz.create({ pregunta: 'Capital de Argentina',
-        	            respuesta: 'Buenos Aires'
+        	            respuesta: 'Buenos Aires',
+                      tema: 'otro'
         	         })
       .then(function(){console.log('Base de datos inicializada')});
      };
